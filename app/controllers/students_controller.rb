11e070c2947c2990f76fe6676553baffe3a4e493
monginadiana <dianahmongina2@gmail.com>
2022-10-21 10:50:30 +0300

    creation of the student/grade and student routing

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,11 @@
+class StudentsController < ApplicationController
+    def index
+        students = Student.all
+        render json: students
+    end
+
+    def grade
+        stds=Student.order('grade desc' )
+        render json: stds
+    end
+end
